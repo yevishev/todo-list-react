ba7c77b86876b6182ebd371dd89646c41791cc4f
yevishev <yevishev.m@gmail.com>
2023-01-06 23:29:52 +0300

    first version todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState('') 
+  const [todos, setTodos] = useState([''])//не понял, почему сюда я нельзя передавать пустой массив
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
+  const clickHandler = () => {
+    todos.push(value)
+    setTodos(todos)
+    setValue('')
+  }
+
+  const removeHandler = () => {
+    setTodos([])
+  }
+
   return (
     <>
-      <h1>hello</h1>
+      <input 
+        type="text" 
+        value={value}
+        placeholder="Enter some todo"
+        onChange={changeHandler}
+      />
+
+      <button onClick={clickHandler}>+</button>
+      <button onClick={removeHandler}>remove all</button>
+      <ul>
+      {todos.map((val, k) => <li style={{listStyleType: 'none'}} key={k}>{val}</li>)}
+      </ul>
     </>
   );
 }
